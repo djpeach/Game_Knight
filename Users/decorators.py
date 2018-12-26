a56495f45bfc936ee5e90abc3f56746ad321429c
Daniel Peach <dpeaches96@gmail.com>
2018-12-25 21:12:00 -0500

    cleaned up decorators

--- Users/decorators.py
+++ Users/decorators.py
@@ -1,11 +1,5 @@
 from functools import wraps
-from urllib.parse import urlparse
-
-from django.conf import settings
-from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import resolve_url
-from django.contrib.auth.decorators import user_passes_test
 
 
 def user_in_group(groups):
@@ -19,24 +13,3 @@ def user_in_group(groups):
 			return func(*args, **kwargs)
 		return check_perms
 	return decorator
-
-	# def check_groups(user):
-	# 	for group in user.groups.values_list('name', flat=True):
-
-
-
-	# def check_perms(user):
-	# 	if isinstance(perm, str):
-	# 		perms = (perm,)
-	# 	else:
-	# 		perms = perm
-	# 	# First check if the user has the permission (even anon users)
-	# 	if user.has_perms(perms):
-	# 		return True
-	# 	# In case the 403 handler should be called raise the exception
-	# 	if raise_exception:
-	# 		raise PermissionDenied
-	# 	# As the last resort, show the login form
-	# 	return False
-	#
-	# return user_passes_test(check_perms, login_url=login_url)
--- Users/views.py
+++ Users/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views import generic
 from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib.auth import decorators as auth_decorators
 from . import forms
 from . import decorators
 
